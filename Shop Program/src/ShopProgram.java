8a9df34b755747d396ccc634c902f6e4914866c6
ebarreto-mendoza <60796295+ebarreto-mendoza@users.noreply.github.com>
2020-03-11 17:52:32 -0700

    Update ShopProgram.java

--- Shop Program/src/ShopProgram.java
+++ Shop Program/src/ShopProgram.java
@@ -3,10 +3,11 @@ public class ShopProgram {
 	
 	public static void intro () {
 		System.out.println("This program supports 4 functions: ");
-		System.out.println("1. Setup Shop ");
-		System.out.println("2. Buy Items ");
-		System.out.println("3. List Items ");
-		System.out.println("4. Checkout  ");
+		System.out.println("\t1. Setup Shop ");
+		System.out.println("\t2. Buy Items ");
+		System.out.println("\t3. List Items ");
+		System.out.println("\t4. Checkout  ");
+		System.out.print("Please choose the function you want: ");
 	}
 	
 	public static void setupShop(){
